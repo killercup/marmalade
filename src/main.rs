c82276fb6b7cb30a15fdb444b87895d3cf468525
Pascal Hertleif <pascal@technocreatives.com>
2022-03-01 12:13:39 +0100

    Fisisks

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::f32::consts::PI;
+
 use bevy::{core::FixedTimestep, prelude::*};
+use heron::prelude::*;
 
 #[cfg(feature = "editor")]
 use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
@@ -6,6 +9,7 @@ use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
 use bevy_editor_pls::prelude::*;
 #[cfg(feature = "dev")]
 use bevy_inspector_egui::WorldInspectorPlugin;
+
 mod thingy;
 use thingy::Thingy;
 
@@ -14,6 +18,8 @@ fn main() {
 
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
+    app.add_plugin(PhysicsPlugin::default());
+    app.insert_resource(Gravity::from(Vec2::ZERO));
 
     #[cfg(feature = "dev")]
     app.add_plugin(WorldInspectorPlugin::new());
@@ -21,16 +27,16 @@ fn main() {
     app.add_plugin(EditorPlugin)
         .add_plugin(FrameTimeDiagnosticsPlugin);
 
-    app.add_startup_system(setup)
-        .add_system_set(
-            SystemSet::new()
-                .with_run_criteria(FixedTimestep::step(1. / 60.))
-                .with_system(thingy::input)
-                .with_system(thingy::mouse_input)
-                .with_system(thingy::u_spin_me),
-        )
-        .register_type::<Thingy>()
-        .run();
+    app.add_startup_system(setup);
+    app.add_system_set(
+        SystemSet::new()
+            .with_run_criteria(FixedTimestep::step(1. / 60.))
+            .with_system(thingy::input)
+            .with_system(thingy::mouse_input)
+            .label("movin"),
+    );
+    app.register_type::<Thingy>();
+    app.run();
 }
 
 fn setup(mut commands: Commands) {
@@ -43,6 +49,7 @@ fn setup(mut commands: Commands) {
     let blox = 10_000_f32;
 
     for x in 1..(blox as usize) {
+        let size = Vec3::new(scale, scale, 0.0);
         commands
             .spawn_bundle(SpriteBundle {
                 transform: Transform {
@@ -51,13 +58,24 @@ fn setup(mut commands: Commands) {
                         (x as f32 % blox.sqrt()).floor() * offset - blox.sqrt() * offset / 2.0,
                         0.0,
                     ),
-                    scale: Vec3::new(scale, scale, 0.0),
+                    scale: size,
                     ..Default::default()
                 },
                 sprite: Sprite {
                     color: Color::hsl(360.0 * x as f32 / blox, 0.5, 0.5),
                     ..Default::default()
                 },
+                ..Default::default()
+            })
+            .insert(RigidBody::Dynamic)
+            .insert(CollisionShape::Cuboid {
+                half_extends: size / 2.,
+                border_radius: None,
+            })
+            .insert(Velocity::from(Vec2::ZERO).with_angular(AxisAngle::new(Vec3::Z, -PI)))
+            .insert(PhysicMaterial {
+                restitution: 1.,
+                density: 5.,
                 ..Default::default()
             })
             .insert(Thingy {
--- src/thingy.rs
+++ src/thingy.rs
@@ -1,4 +1,5 @@
 use bevy::{input::mouse::MouseMotion, prelude::*};
+use heron::Velocity;
 
 #[derive(Component, Reflect, Default, Debug)]
 #[reflect(Component)]
@@ -39,14 +40,13 @@ pub fn input(
 pub fn mouse_input(
     mut motion: EventReader<MouseMotion>,
     windows: Res<Windows>,
-    mut query: Query<(&mut Transform,), With<Thingy>>,
+    mut query: Query<(&mut Velocity, &Transform), With<Thingy>>,
     frame_time: Res<Time>,
 ) {
     let mut totaloffset = Vec3::ZERO;
     let move_speed = 10.;
 
     for ev in motion.iter() {
-        // info!("{ev:?}");
         totaloffset += Vec3::new(ev.delta.x, ev.delta.y, 0.);
     }
 
@@ -62,19 +62,10 @@ pub fn mouse_input(
     };
 
     totaloffset = totaloffset * move_speed * frame_time.delta_seconds();
-    // info!("{cursor_position:?}, {totaloffset:?}");
 
-    for (mut transform,) in query.iter_mut() {
+    for (mut velocity, transform) in query.iter_mut() {
         let distance_from_mouse_pointer = Vec3::distance(cursor_position, transform.translation);
         let influence = 1. - nalgebra_glm::smoothstep(100., 420., distance_from_mouse_pointer);
-        // info!("{totaloffset}, {influence}");
-        transform.translation += totaloffset * influence;
-    }
-}
-
-pub fn u_spin_me(mut query: Query<(&mut Transform, &mut Thingy)>) {
-    for (mut transform, mut thingy) in query.iter_mut() {
-        thingy.rotation = (thingy.rotation + 0.04) % 360.;
-        transform.rotation = Quat::from_rotation_z(thingy.rotation);
+        *velocity = velocity.with_linear(velocity.linear + totaloffset * influence);
     }
 }
