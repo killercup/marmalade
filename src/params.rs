066b8bcd254212182645a923b9d213680f2f455d
Pascal Hertleif <pascal@technocreatives.com>
2022-03-04 12:05:15 +0100

    Chaos params!

--- src/params.rs
+++ src/params.rs
@@ -10,19 +10,25 @@ impl ForceParams {
     pub fn regular() -> Self {
         Self {
             go_home_factor: 150.,
-            go_home_influence: (0., 10.),
+            go_home_influence: (0., 100.),
             keyboard_move_speed: 0.5,
             mouse_move_speed: 400.,
             mouse_influence: (3., 20.),
         }
     }
 
+    pub fn chaos() -> Self {
+        Self {
+            go_home_factor: 30.,
+            go_home_influence: (100., 600.),
+            keyboard_move_speed: 0.5,
+            mouse_move_speed: 400.,
+            mouse_influence: (100., 600.),
+        }
+    }
+
     pub fn go_home_influence(&self, distance: f32) -> f32 {
-        nalgebra_glm::smoothstep(
-            self.go_home_influence.0,
-            self.go_home_influence.1,
-            distance.sqrt(),
-        )
+        nalgebra_glm::smoothstep(self.go_home_influence.0, self.go_home_influence.1, distance)
     }
 
     pub fn mouse_influence(&self, distance_from_mouse_pointer: f32) -> f32 {
